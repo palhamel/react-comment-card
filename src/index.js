9fbcbb5c94459de5cda6fcb4380dc5cfe2f66ffa
Pål Hamel <pal.hamel@gmail.com>
2020-07-23 11:06:33 +0200

    Initial setup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
 
 const App = () => {
   return (
-    <div><h3>hej hej</h3></div>
-  )
-}
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="http://" className="avatar">
+          <img src="" alt="avatar" />
+        </a>
+        <div className="content">
+          <a href="http://" className="author">
+            Sten
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 6:00pm</span>
+          </div>
+          <div className="text">Nice blog post</div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-ReactDOM.render(<App />, document.querySelector('#root'))
+ReactDOM.render(<App />, document.querySelector("#root"));
