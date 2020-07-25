37b9b3a175254a6de47cee773f0e96c089a6986d
Pål Hamel <pal.hamel@gmail.com>
2020-07-25 16:33:00 +0200

    Add ApprovalCard comp to App

--- src/components/ApprovalCard.js
+++ src/components/ApprovalCard.js
@@ -1,12 +1,13 @@
 import React from "react";
 
-export const ApprovalCard = () => {
+export const ApprovalCard = (props) => {
+  // console.log(props.children)
   return (
     <div className="ui card">
-      <div className="content">Are you sure?</div>
+      <div className="content">{props.children}</div>
       <div className="extra content">
         <div className="ui two buttons">
-          <div className="ui basic green button">Aprove</div>
+          <div className="ui basic green button">Approve</div>
           <div className="ui basic red button">Reject</div>
         </div>
       </div>
--- src/index.js
+++ src/index.js
@@ -1,41 +1,51 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import faker from 'faker'
+import faker from "faker";
 
 import { CommentDetail } from "./components/CommentDetail";
-import { ApprovalCard } from './components/ApprovalCard'
+import { ApprovalCard } from "./components/ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <ApprovalCard />
+      <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+          Are you sure?
+        </div>
+      </ApprovalCard>
 
-      <CommentDetail
-        author={faker.internet.userName()}
-        timeAgo="Today at 5:45PM"
-        textField="Nice blog post!"
-        srcImage={faker.image.avatar()}
-      />
-      <CommentDetail
-        author={faker.internet.userName()}
-        timeAgo="Today at 2:45AM"
-        textField="Super!"
-        srcImage={faker.image.avatar()}
-      />
-      <CommentDetail
-        author={faker.internet.userName()}
-        timeAgo="Yesterday at 8:00PM"
-        textField="No way?!"
-        srcImage={faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author={faker.internet.userName()}
+          timeAgo="Today at 5:45PM"
+          textField="Nice blog post!"
+          srcImage={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author={faker.internet.userName()}
+          timeAgo="Today at 2:45AM"
+          textField="Super!"
+          srcImage={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author={faker.internet.userName()}
+          timeAgo="Yesterday at 8:00PM"
+          textField="No way?!"
+          srcImage={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
-
-
-
 
 /*
       <CommentDetail
